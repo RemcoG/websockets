025f55350f8af09cceee9567d9d6814b276ba7cc
Remco Groenenboom <Remco Groenenboom>
2017-04-06 17:12:08 +0200

    maybe fixed port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const body_parser = require('body-parser');
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
 
+var port = process.env.PORT || 3000;
+
 io.on('connection', function (socket) {
 	console.log('connection made');
 });
@@ -25,7 +27,7 @@ app.post('/log', function(req, res){
     res.send('200 OK');
 });
 
-http.listen(process.env.PORT || 3000, function(){
-    console.log('listening on *:' + process.env.PORT || 3000);
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
 
